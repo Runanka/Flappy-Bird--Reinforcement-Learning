779763a287e879afe0616ab93bbdfc9b7d97957e
runanka <runanka@outlook.com>
2020-10-03 00:10:37 +0530

    Jumping Feature of the Bird as well as other UI elements with their functions are added

---
+++ Flappy Bird RL/Assets/scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public Text scoreText;
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver
+    }
+
+    int score = 0;
+    bool gameOver = false;
+
+    public bool GameOver { get { return gameOver; } }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        TapController.OnPlayerDied += OnPlayerDied;
+        TapController.OnPlayerScored += OnPlayerScored;
+    }
+
+    private void OnDisable()
+    {
+        TapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerScored -= OnPlayerScored;
+    }
+
+    void OnPlayerDied()
+    {
+        gameOver = true;
+        SetPageState(PageState.GameOver);
+    }
+
+    void OnPlayerScored()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                break;
+
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                break;
+
+            case PageState.GameOver:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(true);
+                break;
+        }
+    }
+
+
+    public void StartGame()
+    {
+        SetPageState(PageState.None);
+        OnGameStarted();
+        score = 0;
+        gameOver = false;
+    }
+}
---
+++ Flappy Bird RL/Assets/scripts/TapController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
+{
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigid_body;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    private void Start()
+    {
+        rigid_body = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+
+    }
+
+    private void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+    }
+
+    void OnGameStarted()
+    {
+        rigid_body.velocity = Vector3.zero;
+        rigid_body.simulated = true;
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigid_body.velocity = Vector3.zero;
+            rigid_body.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "ScoreZone")
+        {
+            OnPlayerScored();
+        }
+        if(col.gameObject.tag == "DeadZone")
+        {
+            rigid_body.simulated = false;
+            OnPlayerDied();
+        }
+    }
+}
